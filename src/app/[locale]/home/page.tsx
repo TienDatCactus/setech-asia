68c40be5562e162c55fc24743e7e0c7f5a0f4131
TienDat <multivncraft@gmail.com>
2025-05-03 09:12:50 +0700

    finished landing

--- src/app/[locale]/home/page.tsx
+++ src/app/[locale]/home/page.tsx
@@ -17,21 +17,21 @@ const page: React.FC = () => {
       <InfoSection />
       <ServiceSection />
       <TrainingSection />
-      <div className="relative min-h-[400px] w-full bg-[#071afd]">
+      <section className="relative min-h-[25rem] w-full bg-[#071afd]">
         <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#ffffff1a_1px,transparent_1px),linear-gradient(to_bottom,#ffffff1a_1px,transparent_1px)] bg-[size:20px_20px] "></div>
-        <div className="absolute bottom-0 left-0 right-0 top-0 bg-radial-[at_50%_10%] from-[rgba(99,102,241,0.3)] from-40% to-[rgba(15,23,42,1)] to-100%"></div>
-        <div className="absolute bottom-0 left-0 right-0 top-0 max-w-[125rem] mx-auto text-white py-20 px-10">
-          <h1>Let's work together.</h1>
-          <p>
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-radial-[at_50%_10%] from-[rgba(99,102,241,0.3)] from-40% to-[#0f172a] to-100%"></div>
+        <div className="absolute bottom-0 left-0 right-0 top-0 max-w-[125rem] mx-auto text-white py-20 px-10 flex items-center justify-center gap-6 flex-col ">
+          <h1 className="text-6xl font-bold">Let's work together.</h1>
+          <p className="text-2xl font-semibold text-center max-w-[50rem]">
             We are the architects of your operational excellence, bringing
             world-class diagnostic and maintenance solutions to partner in your
             success and build a better future.
           </p>
-          <Button>
+          <Button className="bg-white text-[#0f172a] hover:bg-white hover:scale-110 border-none rounded-none px-14 py-8 text-lg font-semibold group cursor-pointer flex items-center gap-2">
             Start a Project <Power />
           </Button>
         </div>
-      </div>
+      </section>
     </main>
   );
 };
--- src/components/pages/home/TrainingSection.tsx
+++ src/components/pages/home/TrainingSection.tsx
@@ -7,12 +7,18 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+
 import { Plus, Trophy } from "lucide-react";
 
 const TrainingSection: React.FC = () => {
   return (
     <section className="relative h-full w-full min-h-[50rem] bg-[#f5f5f5]">
-      <div className="grid grid-cols-12 max-w-[125rem] mx-auto text-[#0d0e1d] py-20 px-10">
+      <div className="grid grid-cols-12 space-x-4 max-w-[125rem] mx-auto text-[#0d0e1d] py-40 px-10">
         <article className="col-span-6 flex flex-col items-start justify-center gap-4 px-6">
           <div className="flex flex-col items-start justify-center gap-4">
             <h1 className="text-6xl font-bold">
@@ -25,9 +31,37 @@ const TrainingSection: React.FC = () => {
                 <i>ISO 18436-7 standards</i>
               </b>
               . This is an exclusive program developed and led by{" "}
-              <u className="hover:text-[#071afd] cursor-pointer">
-                Mr. Mikael Cronholm
-              </u>
+              <HoverCard >
+                <HoverCardTrigger>
+                  <u className="hover:text-[#071afd] cursor-pointer">
+                    Mr. Mikael Cronholm
+                  </u>
+                </HoverCardTrigger>
+                <HoverCardContent className="w-[28.75rem]">
+                  <div className="grid grid-cols-12 gap-4 items-center">
+                    <div className="col-span-3">
+                      <Image 
+                      src="/images/cronholm.jpg"
+                      alt="Mr. Mikael Cronholm"
+                      className="w-full p-2 object-cover object-center rounded-full border border-[#ccc]"
+                      width={2000}
+                      height={200}
+                      />
+                    </div>
+                    <article className="col-span-9">
+                      <h1 className="text-2xl font-semibold hover:underline">
+                       <a href="https://www.linkedin.com/in/mikael-cronholm-37876415b/" target="_blank"> Mr. Mikael Cronholm</a>
+                      </h1>
+                      <p className="text-md leading-6">
+                      An experienced Swedish Infrared Thermography expert with 33 years in the field, specializing in Furnace Thermal Diagnosis and serving as the developer and instructor for Setech Service's exclusive IRT training program.
+                      </p>
+                      <cite className="text-sm text-gray-500">
+                        Expert in Infrared Thermography
+                      </cite>
+                    </article>
+                  </div>
+                </HoverCardContent>
+              </HoverCard>
               , a world-renowned expert with decades of experience in the field,
               bringing unparalleled practical knowledge to the course.
             </p>
@@ -89,8 +123,8 @@ const TrainingSection: React.FC = () => {
             src="/images/pexels-rezwan-1216589.jpg"
             alt="mentoring"
             className="w-full h-full object-cover object-center group-hover:scale-105 transition-all duration-300 ease-in-out"
-            width={500}
-            height={300}
+            width={2000}
+            height={200}
           />
           <div className="absolute bottom-2 right-2 bg-white p-4 text-[#0d0e1d] group-hover:scale-105 transition-all duration-300 ease-in-out z-100 w-[140px] h-[140px]">
             <div className="bg-[#f5f5f5] w-[50px] h-[50px] rounded-full flex items-center justify-center mb-2">
